3e1f783b324539564f477d1af915a99b6954ed41
yusufgokdemir <71265842+yusufgokdemir@users.noreply.github.com>
2020-11-30 20:26:53 +0100

    Create horoscope.js

---
+++ horoscope.js
@@ -0,0 +1,76 @@
+const month = 3; // mart ayi
+const day = 12; // ayin 12. gunu
+
+if (month == 1) {
+            if (day <= 20) {
+                console.log(" oğlak burcusunuz ");
+                } else {
+                    console.log(" kova burcusunuz ");
+                }
+            }        else if (month == 2) {
+                        if (day <= 19) {
+                            console.log(" kova burcusunuz ");
+                        } else {
+                            console.log(" balık burcusunuz ");
+                        }
+                        } else if (month == 3) {
+                        if (day <= 20) {
+                            console.log(" balık burcusunuz ");
+                        } else {
+                            console.log(" koç burcusunuz ");
+                        }
+                        } else if (month == 4) {
+                        if (day <= 20) {
+                            console.log(" koç burcusunuz ");
+                        } else {
+                            console.log(" boğa burcusunuz ");
+                        }
+                        } else if (month == 5) {
+                        if (day <= 20) {
+                            console.log(" boğa burcusunuz ");
+                        } else {
+                            console.log(" ikizler burcusunuz ");
+                        }
+                        } else if (month == 6) {
+                        if (day <= 21) {
+                            console.log(" ikizler burcusunuz ");
+                        } else {
+                            console.log(" yengeç burcusunuz ");
+                        }
+                        } else if (month == 7) {
+                        if (day <= 23) {
+                            console.log(" yengeç burcusunuz ");
+                        } else {
+                            console.log(" aslan burcusunuz ");
+                        }
+                        } else if (month == 8) {
+                        if (day <= 23) {
+                            console.log(" aslan burcusunuz ");
+                        } else {
+                            console.log(" başak burcusunuz ");
+                        }
+                        } else if (month == 9) {
+                        if (day <= 23) {
+                            console.log(" başak burcusunuz ");
+                        } else {
+                            console.log(" terazi burcusunuz ");
+                        }
+                        } else if (month == 10) {
+                        if (day <= 23) {
+                            console.log(" terazi burcusunuz ");
+                        } else {
+                            console.log(" akrep burcusunuz ");
+                        }
+                        } else if (month == 11) {
+                        if (day <= 22) {
+                            console.log(" akrep burcusunuz ");
+            }       else {
+                console.log(" yay burcusunuz ");
+            }
+            } /*(month == 12)*/ else {
+            if (day <= 21) {
+                console.log(" yay burcusunuz ");
+  } else {
+    console.log(" oğlak burcusunuz ");
+  }
+}
